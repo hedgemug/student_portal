60c6e604ad960f21e3a6463fae2c22543cc5c6bf
Stephen Carico <stephen.r.carico@gmail.com>
2018-03-11 10:01:13 -0700

    initiated html/js login -- needs routing

---
+++ public/index.js
@@ -0,0 +1,56 @@
+/* global Vue, VueRouter, axios */
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        // FILE WILL BE ROUTED WHERE?
+        // .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          console.log(response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+   { path: "/login", component: LoginPage }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
+});
