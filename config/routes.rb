27bd49bc0653751dec5a972a920d811a4b71c3d2
Drew Pilgrim <drew23pilgrim@gmail.com>
2018-03-12 18:41:54 -0700

    Fixed Bugs on Dummy Data

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get '/students' => 'databases#index'
-  get '/experiances' => 'experiances#index'
+  get '/students' => 'students#index'
+  get '/experiences' => 'experiences#index'
   get '/skills' => 'skills#index'
   get '/education' => 'educations#index'
-  get '/capstone' => 'capstone#index'
+  get '/capstones' => 'capstones#index'
 end
 
--- public/index.js
+++ public/index.js
@@ -35,9 +35,48 @@ var LoginPage = {
   }
 };
 
+var ProfilePage = {
+  template: "#profile-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        // FILE WILL BE ROUTED WHERE?
+        // .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          console.log(response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+
+
 var router = new VueRouter({
   routes: [
-   { path: "/login", component: LoginPage }
+   { path: "/login", component: LoginPage },
+   { path: "/profile", component: PorfilePage }
+
   ],
   scrollBehavior: function(to, from, savedPosition) {
     return { x: 0, y: 0 };
