0d8f8753cd5275679a442bfc544b691e19bd879e
Stephen Carico <stephen.r.carico@gmail.com>
2018-03-12 18:43:48 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   get '/teams' => 'teams#index'
   get '/students' => 'databases#index'
-  get '/experiances' => 'experiances#index'
+  get '/experiences' => 'experiences#index'
   get '/skills' => 'skills#index'
   get '/education' => 'educations#index'
-  get '/capstone' => 'capstone#index'
+  get '/capstones' => 'capstones#index'
+
 end
 
