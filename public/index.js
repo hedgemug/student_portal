768bf69ffead045a914fe01f30e88c561114d719
Drew Pilgrim <drew23pilgrim@gmail.com>
2018-03-12 20:35:28 -0700

    update

--- public/index.js
+++ public/index.js
@@ -39,33 +39,43 @@ var ProfilePage = {
   template: "#profile-page",
   data: function() {
     return {
-      email: "",
-      password: "",
+      currentStudent: {user_name: "Hello"},
       errors: []
     };
   },
-  methods: {
-    submit: function() {
-      var params = {
-        auth: { email: this.email, password: this.password }
-      };
-      axios
-        // FILE WILL BE ROUTED WHERE?
-        // .post("/user_token", params)
-        .then(function(response) {
-          axios.defaults.headers.common["Authorization"] =
-            "Bearer " + response.data.jwt;
-          localStorage.setItem("jwt", response.data.jwt);
-          console.log(response.data.jwt);
-          router.push("/");
-        })
-        .catch(
+  created: function() {
+    console.log("Running Twice?");
+    axios.get("http://localhost:3000/students").then(function(response){
+       this.currentStudent = response.data[0];
+       console.log(this.currentStudent);
+        console.log(Object.keys(this.currentStudent));
+      }.bind(this)).catch(
           function(error) {
-            this.errors = ["Invalid email or password."];
-            this.email = "";
-            this.password = "";
+            this.errors = error.response.data.errors;
           }.bind(this)
         );
+      function data(){ 
+      }
+  },
+  methods: {
+    update: function() {
+      axios
+        .patch("/students/" + this.currentContactID, this.currentStudent)
+        .then(function(response) {
+           console.log("Updated!");
+        }.bind(this)
+        ).catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    },
+    output: function(input){
+      if(input != "photo"){
+        return true;
+      }else{
+        return false;
+      }
     }
   }
 };
@@ -75,7 +85,7 @@ var ProfilePage = {
 var router = new VueRouter({
   routes: [
    { path: "/login", component: LoginPage },
-   { path: "/profile", component: PorfilePage }
+   { path: "/profile", component: ProfilePage }
 
   ],
   scrollBehavior: function(to, from, savedPosition) {
